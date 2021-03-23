0e8abbbfa09f7daaac6b96a13c88078b21f74304
Fatihcil16 <fatihcil.2001@gmail.com>
2021-03-23 18:38:51 +0300

    Day 20

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -77,8 +77,9 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.ConfigureCustomExceptionMiddleware();
 
-            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
